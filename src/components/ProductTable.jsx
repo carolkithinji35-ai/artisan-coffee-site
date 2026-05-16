05a22aabe0ba94e82b77bc437af96fed821e0c99
Carol Kithinji <carolkithinji35@gmail.com>
2026-05-16 16:38:39 +0300

    add handle delete

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -1,10 +1,27 @@
-export default function ProductTable({ coffees }) {
+export default function ProductTable({ coffees, setCoffees }) {
+
+  //handle deleting a coffee, use delete request to backend(json.server), that removes coffee from db.json using its id.
+  // updates local state
+
+  //
+  function handleDelete(id) {
+    fetch(`http://localhost:3001/coffees/${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      setCoffees(coffees.filter((coffee) => coffee.id !== id));
+    });
+  }
+
   return (
     <div className="mt-6 md:max-w-2/3 mx-auto">
       <ul className="space-y-2">
         {coffees.map((coffee) => (
-          <li key={coffee.id} className="p-2 border  bg-white shadow-sm rounded-lg border-stone-100 hover:shadow-md transition">
-            {coffee.name}
+          <li
+            key={coffee.id}
+            className="p-3 border  bg-white shadow-sm rounded-lg border-stone-100 hover:shadow-md transition flex justify-between items-center"
+          >
+            <span> {coffee.name}</span>
+            <button onClick={()=>handleDelete(coffee.id)} className="text-red-600 hover:text-red-800">Delete</button>
           </li>
         ))}
       </ul>
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -8,15 +8,43 @@ export default function Admin() {
   const [description, setDescription] = useState("");
   const [location, setLocation] = useState("");
   const [image, setImage] = useState("");
-
-
-
-
+  //fetch data from db.json server
   useEffect(() => {
     fetch("http://localhost:3001/coffees")
       .then((res) => res.json())
       .then((data) => setCoffees(data));
   });
+
+  // handle new coffee, prevent refresh
+  // send request to json-server, use Post method to collect and add new coffee data and stringify to convert js object into json format..after, saves new coffee in db.json
+  // after fetch, form clears nd prepares for next entry.
+  const handleCoffee = (event) => {
+    event.preventDefault();
+    const newCoffee = {
+      name,
+      price,
+      location,
+      image,
+      description,
+    };
+
+    fetch("http://localhost:3001/coffees", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newCoffee),
+    }).then((addedCoffee) => {
+      //uses the latest state
+    setCoffees((prev)=> [...prev, addedCoffee])
+      setName("");
+      setPrice("");
+      setLocation("");
+      setImage("");
+      setDescription("");
+    });
+  };
+
   return (
     <div className="min-h-screen   p-10 bg-stone-100">
       <h1 className="text-4xl font-bold text-amber-900 mb-6 text-center">
@@ -25,7 +53,8 @@ export default function Admin() {
       </h1>
       <form
         action=""
-        className="mt-6 space-y-4 max-w-md bg-white p-6 rounded-2xl shadow mx-auto"
+        onSubmit={handleCoffee}
+        className="mt-6 space-y-4 max-w-md bg-white p-6 rounded-2xl shadow-lg mx-auto"
       >
         <input
           type="text"
@@ -35,7 +64,7 @@ export default function Admin() {
           onChange={(e) => setName(e.target.value)}
         />
         <input
-          type="text"
+          type="number"
           className="w-full px-4 py-3 rounded-lg border border-stone-200 bg-white shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-700"
           placeholder="Price"
           value={price}
@@ -66,7 +95,7 @@ export default function Admin() {
           Add
         </button>
       </form>
-      <ProductTable coffees={coffees} />
+      <ProductTable coffees={coffees} setCoffees={setCoffees} />
     </div>
   );
 }
